59052014b994c984041da1d5289085764f38bd23
Oluchi Queen Iheagwara <oluchiqueeniheagwara@gmail.com>
2025-08-08 15:33:04 +0100

    Hero section

---
+++ todo-app/src/components/Hero_Section/Calender.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Test Event",
+    start: new Date(),
+    end: new Date(),
+    allDay: true,
+  },
+];
+
+function MyCalendar() {
+  return (
+    <div style={{ height: "500px", padding: "20px" }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
+
+export default MyCalendar;
--- todo-app/src/components/Hero_Section/Hero.jsx
+++ todo-app/src/components/Hero_Section/Hero.jsx
@@ -1,21 +1,99 @@
-import React, { useState } from 'react'
-import "../Hero_Section/Hero.css"
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "../Hero_Section/Hero.css";
 
 function Hero() {
   const [date, setDate] = useState(new Date());
 
   return (
     <div>
-      <div className='welcome_message'>
-        <h1>Hello Oluchi, <span>Start Planning Today...</span></h1>
+      <div className="welcome_message">
+        <h1>
+          Hello Oluchi, <span>Start Planning Today...</span>
+        </h1>
       </div>
-      <div className='main_content'></div>
-      <div>Selected date: {date.toDateString()}</div>
-      <Calendar onChange={setDate} value={date} />
+
+      <div className="main_section">
+        {/* Adding the react-calendar to our code */}
+        <div>
+          <div className="todays_date"><span>Today's Date:</span> {date.toDateString()}</div>
+          <Calendar onChange={setDate} value={date} />
+        </div>
+
+        <div>
+          <div className="todo_input">
+            <div>
+            <input type="text" placeholder="Type Title Of Task"/>
+            <input type="text" placeholder="Details Of Your Task"/>
+            </div>
+            <div>
+            <button>+</button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h3>Learn Javascript</h3>
+              <p>Master the learning powering the modern web.</p>
+              <h4>Start date: 12-08-2025</h4>
+            </div>
+            <div>
+              <img src="" alt="Completed Task" />
+              <img src="" alt="Edit Task" />
+              <img src="" alt="Delete Task" />
+            </div>
+
+            <div>
+              <h3>Learn Javascript</h3>
+              <p>Master the learning powering the modern web.</p>
+              <h4>Start date: 12-08-2025</h4>
+            </div>
+            <div>
+              <img src="" alt="Completed Task" />
+              <img src="" alt="Edit Task" />
+              <img src="" alt="Delete Task" />
+            </div>
+
+            <div>
+              <h3>Learn Javascript</h3>
+              <p>Master the learning powering the modern web.</p>
+              <h4>Start date: 12-08-2025</h4>
+            </div>
+            <div>
+              <img src="" alt="Completed Task" />
+              <img src="" alt="Edit Task" />
+              <img src="" alt="Delete Task" />
+            </div>
+
+            <div>
+              <h3>Learn Javascript</h3>
+              <p>Master the learning powering the modern web.</p>
+              <h4>Start date: 12-08-2025</h4>
+            </div>
+            <div>
+              <img src="" alt="Completed Task" />
+              <img src="" alt="Edit Task" />
+              <img src="" alt="Delete Task" />
+            </div>
+          </div>
+
+          <button>Load More</button>
+
+          <div>
+            <div>
+              <p>COMPLETED TASK</p>
+              <h4>04</h4>
+            </div>
+
+            <div>
+              <p>PENDING TASK</p>
+              <h4>14</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
